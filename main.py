e574c919b7c0d9b348a5abc8d3915412818c2911
Roman <35300872+monaxovdulov@users.noreply.github.com>
2023-02-10 17:55:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""
+DESCRIPTION_GAME = """
 Описание программы "Игра в слова"
 
 Играют два игрока
@@ -15,13 +15,28 @@
 или пока у игроков положительный счёт
 
 """
+print(DESCRIPTION_GAME)
 
 # здесь будет хранится количесво очков у игроков
 score_player1 = 10
 score_player2 = 10
 
-user_word = input('игрок 1 начинает игру и вводит любое слово\n чтобы выйти из игры введите команду stop:').lower()
 print(f"Счёт игроков\nИгрок 1 - {score_player1}\nИгрок 2 - {score_player2}\n")
+user_word = input('игрок 1 начинает игру и вводит любое слово\n чтобы выйти из игры введите команду stop:').lower()
+
+while user_word == "":
+    print("Ты не ввел никакого слова попробуй ещё раз")
+    user_word = input('Вводить сюда:').lower()
+
+is_an_invalid_char = True
+while is_an_invalid_char:
+    for char in user_word:
+        if char not in 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя':
+            print(f"Недопустимый символ {char} ")
+            user_word = input('Введите слово еще\nВводить сюда:').lower()
+            break
+    else:
+        is_an_invalid_char = False
 
 used_words = [user_word]  # список уже использованных слов, сразу создаем с первым словом word
 counter = 1  # перемнная счётчик, для того чтобы в цикле игроки шли по очереди
@@ -29,6 +44,10 @@ counter = 1  # перемнная счётчик, для того чтобы в 
 # код ниже будет выполняться до тех пор пока игрок не введет слово "stop" или пока у игроков положительный счёт
 while user_word != "stop" or (score_player1 > 0 and score_player2 > 0):
     print("Слова которые уже были", used_words)
+    while user_word == "":
+        print("Ты не ввел никакого слова попробуй ещё раз")
+        user_word = input('Вводить сюда:').lower()
+
     last_letter = user_word[-1]  # делаем срез строки по индексу [-1]  чтобы получить последнюю букву
     if not (last_letter in "ячсмитбюэждлорпавфйцукенгшщзхё"):  # если последняя буква не входит в "ячсм..."
         last_letter = user_word[-2]  # то в берем предпоследнюю букву
@@ -74,11 +93,9 @@ while user_word != "stop" or (score_player1 > 0 and score_player2 > 0):
 
     print(f"Счёт игроков\nИгрок 1 - {score_player1}\nИгрок 2 - {score_player2}\n")
 
-
 if score_player1 > 0 and score_player1 > score_player2:
     print(f"Победил игрок 1\nЕго счёт - {score_player1}")
 elif score_player2 > 0 and score_player2 > score_player1:
     print(f"Победил игрок 2\nЕго счёт - {score_player2}")
 else:
     print(f"игрок 1 {score_player1}\nигрок 2{score_player2}")
-    
